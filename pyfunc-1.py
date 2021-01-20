1d98dc95fb9101909ef0f799e9a5f6b5acecbdf7
cam-em <cmoreno.1117@gmail.com>
2021-01-20 17:38:26 -0600

    added py file

---
+++ pyfunc-1.py
@@ -0,0 +1,31 @@
+
+# Challenge 1
+# def sum_to(n):
+#     sum = 0
+#     for i in range(1, n + 1):
+#         sum += i
+    
+#     print(sum)
+
+# Challenge 2
+# def largest(list):
+#     print(max(list))
+
+# largest([1,2,3,4,5,6,7])
+
+# Challenge 3
+# def occurances(string, letters):
+#     print(string.count(letters))
+    
+# occurances('fleep floop', 'e')   # returns 2
+# occurances('fleep floop', 'p')   # returns 2
+# occurances('fleep floop', 'ee')  # returns 1
+# occurances('fleep floop', 'fe')  # returns 0
+
+# def product(*vals):
+#     product = 1
+#     for val in vals:
+#         product *= val
+#     print(product)
+
+# product(1,2,3)
